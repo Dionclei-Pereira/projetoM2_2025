4bb087d662a74b49106035926ac4bdd7e0a80b6a
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-11-29 16:15:21 -0300

    Criada classe abstrata Pessoa

---
+++ src/entidades/Pessoa.java
@@ -0,0 +1,16 @@
+package entidades;
+
+public abstract class Pessoa {
+
+    private String nome;
+    private String cpf;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+}
